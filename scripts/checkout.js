a01a5b3d37365b4ff651d941341ab4817c6cd0ab
believe037 <engr.believenduka@gmail.com>
2025-02-11 10:51:42 +0100

    delete button in checkout interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { removeFromCart } from "../data/cart.js";
 
 let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
@@ -38,7 +39,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -94,3 +95,12 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    // Remove product from page using data-productid
+    const productId = link.dataset.productId;
+    removeFromCart(productId)
+    console.log(cart)
+  })
+})
