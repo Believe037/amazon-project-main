72885d24bc56ffc1cc249524fe18d0ce3b295616
believe037 <engr.believenduka@gmail.com>
2025-02-21 12:07:04 +0100

    load products from a backend

--- data/products.js
+++ data/products.js
@@ -73,6 +73,28 @@ console.log(date.toLocaleDateString('en-US'));
 
 // object3.method();
 
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    fun()
+    // console.log('load products')
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -769,4 +791,5 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
-});
+}); 
+*/
